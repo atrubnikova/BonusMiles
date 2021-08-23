37c53b37f4d77ff1f7104878c09e48de24c89718
atrubnikova <sashk@ngs.ru>
2021-08-23 18:44:40 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 8579;
+        int oneMile = 20;
+
+        long bonus = price / oneMile;
+        System.out.println(bonus);
+    }
+}
